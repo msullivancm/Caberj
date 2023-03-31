0c0161a8961ebea526668217afd76278d20c68eb
msullivancm <marcus.sullivan@gmail.com>
2023-03-31 17:53:36 -0300

    .

--- Plano de Saude/WebService/API_REST/apirest.py
+++ Plano de Saude/WebService/API_REST/apirest.py
@@ -4,23 +4,39 @@ Baixar e instalar https://visualstudio.microsoft.com/pt-br/visual-cpp-build-tool
     Instalar o pacote: .net desktop build tools ou ferramentas de build de área de trabalho do .net
     (Microsoft Visual C++ 14.0 or greater is required. Get it with "Microsoft C++ Build Tools": https://visualstudio.microsoft.com/visual-cpp-build-tools/)
 pip3 install flask flask-cors
-pip3 install cx_Oracle --upgrade
+pip3 install pyproject.toml
+python3 -m pip install cx_Oracle --upgrade
+conda install cx_Oracle
+
+python -m pip install oracledb --upgrade
+
+
 Para executar a api rest '''
 #python3 -m flask -A "C:\Users\Sullivan\source\Caberj\Plano de Saude\WebService\API_REST\apirest.py" run
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import oracledb
+import os
 
-app = Flask(__name__)
+""" app = Flask(__name__)
 app.run(debug=True, port=8080)
-CORS(app)
+CORS(app) """
 
 def oracle_connection():
-    import cx_Oracle
-    con = cx_Oracle.connect('siga/taqui0pA@10.19.1.117:1521/caberj')
-    return con
+    oracledb.init_oracle_client(lib_dir=r"C:\Users\Sullivan\source\Caberj\instantclient_21_9")
+    un = os.environ.get('ORACLE_USER')
+    pw = os.environ.get('ORACLE_PASSWORD')
+    cs = os.environ.get('ORACLE_CONNECTION_STRING')
+    with oracledb.connect(user="siga", password="taqui0pA", host="10.19.1.117", port="1521", service_name="caberj") as connection:
+        with connection.cursor() as cursor:
+            sql = """select sysdate from dual"""
+            for r in cursor.execute(sql):
+                print(r)
+    """ con = cx_Oracle.connect('siga/taqui0pA@10.19.1.117:1521/caberj') """
+    return
 
-@app.route('/', methods=['GET'])
+#@app.route('/', methods=['GET'])
 def api_all():
     con = oracle_connection()
     cur = con.cursor()
@@ -29,3 +45,4 @@ def api_all():
     con.close() 
     return jsonify(rows)
 
+print(oracle_connection()) 
